51cff85fb69cf11a82a473c8569d4a1880b3cb9a
[200~krishnajk18 <jeyakrishna333@gmail.com>
2022-11-24 09:52:39 +0530

    code has been done

---
+++ src/org/Git/ExampleGitHub.java
@@ -0,0 +1,12 @@
+package org.Git;
+
+public class ExampleGitHub {
+	
+	public static void main(String[] args) {
+		
+	}
+       private void employeename() {
+		System.out.println("name of emmployee:karan");
+
+	}
+}
