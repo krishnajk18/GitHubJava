e6a72b04e982885b031f1d313b5d4166f03d830b
[200~krishnajk18 <jeyakrishna333@gmail.com>
2022-11-24 11:53:28 +0530

    added coding

--- src/org/Git/ExampleGitHub.java
+++ src/org/Git/ExampleGitHub.java
@@ -1,5 +1,7 @@
 package org.Git;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 public class ExampleGitHub {
 	
 	public static void main(String[] args) {
@@ -7,6 +9,13 @@ public class ExampleGitHub {
 	}
        private void employeename() {
 		System.out.println("name of emmployee:karan");
+		
+		int a = 5;
+		int b = 6;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
 
 	}
 }
